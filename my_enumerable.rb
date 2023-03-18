a81143c189f254e1ba875ea471179741a826b2a8
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-03-19 01:10:03 +0200

    Clear code and fix linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,4 +1,4 @@
-module
+module MyEnumerable
   def all?(&block)
     each { |item| return false unless block.call(item) }
     true
@@ -14,5 +14,4 @@ module
     each { |item| result << item if block.call(item) }
     result
   end
-
 end
